c11e3f66758e6fb0e2fe9838fc1bff3251caebea
AmareGatie <amaregatie2006@gmail.com>
2023-01-06 00:53:29 -0600

    How to make a script dynamic!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the number of and list of arguments."""
+    """Hello Welcome To The Best School."""
     import sys
-
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
