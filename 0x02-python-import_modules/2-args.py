da9556e2e1dc1170dcd781ce8739a861cc1b55ee
AmareGatie <amaregatie2006@gmail.com>
2023-01-08 11:17:08 -0600

    task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """Hello Welcome To The Best School."""
     import sys
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
+    if len(sys.argv) - 1 == 0:
+        print("{} arguments.".format(len(sys.argv) - 1))
+    elif len(sys.argv) - 1 == 1:
+        print("{} argument:".format(len(sys.argv) - 1))
+        print("{}: {}".format(len(sys.argv) - 1, sys.argv[len(sys.argv) - 1]))
     else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
