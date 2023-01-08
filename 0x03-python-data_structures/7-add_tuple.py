82a9b15fdbe9af868a76902f652b8bc9f44f2a89
AmareGatie <amaregatie2006@gmail.com>
2023-01-08 10:57:51 -0600

    task solved

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,4 +5,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_2 = tuple_b + (0, 0)
     new_tuple = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
     return new_tuple
-
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -8,4 +8,3 @@ def max_integer(my_list=[]):
             if my_list[i] > max:
                 max = my_list[i]
         return max
-
